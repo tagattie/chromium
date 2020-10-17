53ccbe5e50ba3065a57fd00c4d164ee11a2f4d38
Rene Ladan <rene0@freedom.nl>
2020-10-17 12:56:33 +0200

    Rename and adjust some patches for which the target moved.

--- www/chromium/files/patch-sandbox_policy_switches.cc
+++ www/chromium/files/patch-sandbox_policy_switches.cc
@@ -1,11 +1,11 @@
---- services/service_manager/sandbox/switches.cc.orig	2020-09-08 12:14:09.000000000 -0700
-+++ services/service_manager/sandbox/switches.cc	2020-09-17 12:18:17.445443000 -0700
-@@ -85,7 +85,7 @@
+--- sandbox/policy/switches.cc.orig	2020-10-07 16:38:49 UTC
++++ sandbox/policy/switches.cc
+@@ -80,7 +80,7 @@ const char kGpuSandboxFailuresFatal[] = "gpu-sandbox-f
  // Meant to be used as a browser-level switch for testing purposes only.
  const char kNoSandbox[] = "no-sandbox";
  
--#if defined(OS_LINUX)
-+#if defined(OS_LINUX) || defined(OS_BSD)
+-#if defined(OS_LINUX) || defined(OS_CHROMEOS)
++#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_BSD)
  // Instructs the zygote to launch without a sandbox. Processes forked from this
  // type of zygote will apply their own custom sandboxes later.
  const char kNoZygoteSandbox[] = "no-zygote-sandbox";
