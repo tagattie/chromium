90cff5ab88fbf8c4ac8397028ed5d9dd1d530757
Joseph Mingrone <jrm@ftfl.ca>
2020-11-21 18:25:31 -0400

    Fix video_encoder.cc patch, which was no longer applying cleanly

--- www/chromium/files/patch-third__party_blink_renderer_modules_webcodecs_video__encoder.cc
+++ www/chromium/files/patch-third__party_blink_renderer_modules_webcodecs_video__encoder.cc
@@ -1,11 +1,11 @@
---- third_party/blink/renderer/modules/webcodecs/video_encoder.cc.orig	2020-11-16 14:54:56 UTC
+--- third_party/blink/renderer/modules/webcodecs/video_encoder.cc.orig	2020-11-17 18:39:30 UTC
 +++ third_party/blink/renderer/modules/webcodecs/video_encoder.cc
 @@ -53,7 +53,7 @@ namespace {
  std::unique_ptr<media::VideoEncoder> CreateAcceleratedVideoEncoder(
      media::VideoCodecProfile profile,
      const media::VideoEncoder::Options& options) {
--#if defined(OS_MAC) || defined(OS_LINUX)
-+#if defined(OS_MAC) || defined(OS_LINUX) || defined(OS_BSD)
+-#if defined(OS_MAC) || (defined(OS_LINUX) && !defined(OS_CHROMEOS))
++#if defined(OS_MAC) || (defined(OS_LINUX) || defined(OS_BSD) && !defined(OS_CHROMEOS))
    // TODO(https://crbug.com/1110279) Flush() is not implemented on MacOS'
    // accelerated video encoder, so we can't use it yet.
    return nullptr;
@@ -13,8 +13,8 @@
        media::AsyncDestroyVideoEncoder<media::VideoEncodeAcceleratorAdapter>>(
        std::make_unique<media::VideoEncodeAcceleratorAdapter>(
            gpu_factories, std::move(task_runner)));
--#endif  // defined(OS_MAC) || defined(OS_LINUX)
-+#endif  // defined(OS_MAC) || defined(OS_LINUX) || defined(OS_BSD)
+-#endif  // defined(OS_MAC) || (defined(OS_LINUX) && !defined(OS_CHROMEOS))
++#endif  // defined(OS_MAC) || (defined(OS_LINUX) || defined(OS_BSD) && !defined(OS_CHROMEOS))
  }
  
  std::unique_ptr<media::VideoEncoder> CreateVpxVideoEncoder() {
