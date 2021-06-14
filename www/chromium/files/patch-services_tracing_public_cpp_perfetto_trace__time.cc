07967255494fde05cf61997827b2919c59ce9f98
Matthias Wolf <freebsd@rheinwolf.de>
2021-06-14 14:25:15 +0200

    Fixed tracing patch

--- www/chromium/files/patch-services_tracing_public_cpp_perfetto_trace__time.cc
+++ www/chromium/files/patch-services_tracing_public_cpp_perfetto_trace__time.cc
@@ -1,11 +1,11 @@
---- services/tracing/public/cpp/perfetto/trace_time.cc.orig	2021-06-11 09:59:26 UTC
+--- services/tracing/public/cpp/perfetto/trace_time.cc.orig	2021-06-14 12:19:41 UTC
 +++ services/tracing/public/cpp/perfetto/trace_time.cc
 @@ -12,7 +12,7 @@ namespace tracing {
  
  int64_t TraceBootTicksNow() {
    // On Windows and Mac, TRACE_TIME_TICKS_NOW() behaves like boottime already.
--#if defined(OS_BSD) || defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_ANDROID) || \
-+#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_ANDROID) || \
+-#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_ANDROID) || \
++#if defined(OS_BSD) || defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_ANDROID) || \
      defined(OS_FUCHSIA)
    struct timespec ts;
    int res = clock_gettime(CLOCK_BOOTTIME, &ts);
@@ -14,5 +14,5 @@
  }
  
 -}  // namespace tracing
-+}  // namespace tracing
 \ No newline at end of file
++}  // namespace tracing
