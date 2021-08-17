6761fa3b198abe0c698fc888582a78f23acbdd86
Matthias Wolf <freebsd@rheinwolf.de>
2021-08-17 13:26:22 +0200

    Update to 92.0.4515.159 and build with Python 3

--- www/chromium/files/patch-chrome_browser_extensions_api_erp_chrome_desktop_report_request_helper.cc
+++ www/chromium/files/patch-chrome_browser_extensions_api_erp_chrome_desktop_report_request_helper.cc
@@ -1,4 +1,4 @@
---- chrome/browser/extensions/api/enterprise_reporting_private/chrome_desktop_report_request_helper.cc.orig	2021-07-19 18:45:08 UTC
+--- chrome/browser/extensions/api/enterprise_reporting_private/chrome_desktop_report_request_helper.cc.orig	2021-08-17 00:15:57 UTC
 +++ chrome/browser/extensions/api/enterprise_reporting_private/chrome_desktop_report_request_helper.cc
 @@ -23,7 +23,7 @@
  #include "base/win/registry.h"
--- www/chromium/files/patch-weblayer_browser_browser__main__parts__impl.cc
+++ www/chromium/files/patch-weblayer_browser_browser__main__parts__impl.cc
@@ -1,6 +1,6 @@
---- weblayer/browser/browser_main_parts_impl.cc.orig	2021-07-19 18:45:45 UTC
+--- weblayer/browser/browser_main_parts_impl.cc.orig	2021-08-17 00:17:09 UTC
 +++ weblayer/browser/browser_main_parts_impl.cc
-@@ -78,7 +78,7 @@
+@@ -79,7 +79,7 @@
  #endif
  // TODO(crbug.com/1052397): Revisit once build flag switch of lacros-chrome is
  // complete.
@@ -9,7 +9,7 @@
  #include "ui/base/ime/init/input_method_initializer.h"
  #endif
  
-@@ -197,7 +197,7 @@ int BrowserMainPartsImpl::PreEarlyInitialization() {
+@@ -202,7 +202,7 @@ int BrowserMainPartsImpl::PreEarlyInitialization() {
  
  // TODO(crbug.com/1052397): Revisit once build flag switch of lacros-chrome is
  // complete.
