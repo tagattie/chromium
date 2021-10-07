6f81b9258b95fccb71c1429cb3aa0096f388e49c
Matthias Wolf <freebsd@rheinwolf.de>
2021-10-07 19:04:27 +0000

    Fixed xproto generation on 12.2

--- www/chromium/files/patch-ui_gfx_x_gen__xproto.py
+++ www/chromium/files/patch-ui_gfx_x_gen__xproto.py
@@ -1,6 +1,15 @@
---- ui/gfx/x/gen_xproto.py.orig	2021-10-05 16:55:56 UTC
-+++ ui/gfx/x/gen_xproto.py
-@@ -1313,6 +1313,7 @@ class GenXproto(FileWriter):
+--- ui/gfx/x/gen_xproto.py.orig	2021-10-01 01:37:23.000000000 +0000
++++ ui/gfx/x/gen_xproto.py	2021-10-07 18:24:11.923085000 +0000
+@@ -1219,6 +1219,8 @@
+         self.write('#include <cstring>')
+         self.write('#include <vector>')
+         self.write()
++        self.write('#include <unistd.h>')
++        self.write()
+         self.write('#include "base/component_export.h"')
+         self.write('#include "base/memory/ref_counted_memory.h"')
+         self.write('#include "base/memory/scoped_refptr.h"')
+@@ -1313,6 +1315,7 @@
          self.write()
          self.write('#include <xcb/xcb.h>')
          self.write('#include <xcb/xcbext.h>')
